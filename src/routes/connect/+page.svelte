3a4992a9b051156b41a2ccdc15ddbe3b64784a0d
Dan Lubbers <lubbers.dan@gmail.com>
2022-09-29 19:33:44 -0400

    Added photography to Connect and made a styling changes.

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -2,7 +2,7 @@
 	import socialMedia from '$lib/data/connect.json';
 </script>
 
-<div class="w-full flex flex-col justify-start">
+<div class="w-full mb-5 flex flex-col justify-start">
 	<p class="pl-5">
 		Email:{` `}
 		<a href="mailto:lubbers.dan@gmail.com?subject=Mailed from danlubbersphotographs.com">
@@ -12,7 +12,7 @@
 	<div class="w-full pt-4 px-10 flex justify-start">
 		<ul>
 			{#each socialMedia as { name, url }}
-				<li class="my-1"><a href={url} target="_blank" rel="noreferrer">{name}</a></li>
+				<li class="my-1 text-xl"><a href={url} target="_blank" rel="noreferrer">{name}</a></li>
 			{/each}
 		</ul>
 	</div>
