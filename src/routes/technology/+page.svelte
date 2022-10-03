36f51210f1320477a73970b29061c79de87af65a
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-03 19:17:16 -0400

    forgot to remove margin from Technology page.

--- src/routes/technology/+page.svelte
+++ src/routes/technology/+page.svelte
@@ -2,7 +2,7 @@
 	import technologies from '$lib/data/technology.json';
 </script>
 
-<div class="mt-10 w-full h-1/2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 justify-items-center">
+<div class="w-full h-1/2 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-5 justify-items-center">
 	{#each technologies as { name, logo }}
 		<div class="my-6 flex flex-col justify-center items-center">
 			<img width="50" src={logo} alt={name} />
