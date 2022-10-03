7c41e551ded69fa7e577e5b7f2c5434ca0fdae21
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-03 18:28:45 -0400

    Created a Codewars component in case I want to use it somewhere on the site.

---
+++ src/lib/components/Codewars.svelte
@@ -0,0 +1,11 @@
+<script>
+	import connect from '$lib/data/connect.json';
+
+	const codewars = connect.find((connect) => connect.name === 'Codewars');
+</script>
+
+<div class="mb-6 w-full flex justify-center">
+	<a href={codewars?.url} aria-label="codewars" target="_blank" rel="noopener noreferrer">
+		<img width="400" src="https://www.codewars.com/users/danlubbers/badges/large" alt="" />
+	</a>
+</div>
