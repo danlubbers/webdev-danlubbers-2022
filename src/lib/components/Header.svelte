8a384ce43393104554963adb2a5a33f7835236fd
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-03 12:38:29 -0400

    Refactored lists to dynamicallly render from an array of objects vs duplicate hardcoded page routes.

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,16 +1,35 @@
 <script>
 	import Logo from '$lib/logos/Logo.svelte';
+	import { page } from '$app/stores';
+
+	const nav = [
+		{ title: 'PROJECTS', path: '/projects' },
+		{ title: 'EDUCATION', path: '/education' },
+		{ title: 'TECHNOLOGY', path: '/technology' },
+		{ title: 'CONNECT', path: '/connect' }
+	];
 </script>
 
-<header class="relative w-full h-18 mb-12 px-8 pt-8 flex flex-row justify-between items-end z-10">
-	<a href="/" class="w-full"><Logo width="3500" /></a>
+<header
+	class="relative w-full mb-12 md:px-8 px-8 pt-8 pr-2 flex flex-row md:justify-between md:items-end justify-center z-10"
+>
+	<a class="w-full max-w-md" href="/">
+		<Logo width="2000" />
+	</a>
 
 	<nav class="md:flex hidden">
 		<ul class="list-reset md:flex md:items-center ">
-			<li class="md:ml-4"><a href="/projects">PROJECTS</a></li>
-			<li class="md:ml-4"><a href="/education">EDUCATION</a></li>
-			<li class="md:ml-4"><a href="/technology">TECHNOLOGY</a></li>
-			<li class="md:ml-4"><a href="/connect">CONNECT</a></li>
+			{#each nav as { title, path }}
+				<li class="md:ml-4">
+					<a class:active={$page.url.pathname === path} href={path}>{title}</a>
+				</li>
+			{/each}
 		</ul>
 	</nav>
 </header>
+
+<style>
+	.active {
+		color: #a52a25;
+	}
+</style>
