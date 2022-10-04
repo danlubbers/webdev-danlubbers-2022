b13cbf2e0e26103cbb7ec719dd33fa28962eead8
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-04 14:37:42 -0400

    Added SEO component to svelte head.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,15 @@
 <script>
 	import '../app.css';
+	import SEO from '$lib/components/SEO.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import Footer from '$lib/components/Footer.svelte';
+
+	const pageTitle = 'Dan Lubbers | Web Developer';
+	const metadescription = 'Dan Lubbers is a Web Developer, Photographer, & Retoucher';
+	const siteLanguage = 'en';
 </script>
 
+<SEO {pageTitle} {metadescription} {siteLanguage} />
 <Header />
 
 <main class="my-8 mx-10 h-screen grid-rows-auto flex justify-center">
