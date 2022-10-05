a8126499ca5fb465954c5591111f711301b775ff
Dan Lubbers <lubbers.dan@gmail.com>
2022-10-05 12:10:53 -0400

    Svelte Font Awesome Icons package has been installed. Icons are now rendering to the appropriate social media links on the connect page.

--- src/routes/connect/+page.svelte
+++ src/routes/connect/+page.svelte
@@ -1,5 +1,29 @@
 <script>
 	import socialMedia from '$lib/data/connect.json';
+	import Icon from 'svelte-awesome';
+	import githubSquare from 'svelte-awesome/icons/githubSquare';
+	import linkedinSquare from 'svelte-awesome/icons/linkedinSquare';
+	import twitter from 'svelte-awesome/icons/twitter';
+	import instagram from 'svelte-awesome/icons/instagram';
+	import camera from 'svelte-awesome/icons/camera';
+	import cog from 'svelte-awesome/icons/cog';
+
+	const logoLoop = (logo) => {
+		switch (logo) {
+			case 'githubSquare':
+				return githubSquare;
+			case 'linkedinSquare':
+				return linkedinSquare;
+			case 'twitter':
+				return twitter;
+			case 'instagram':
+				return instagram;
+			case 'camera':
+				return camera;
+			default:
+				return cog;
+		}
+	};
 </script>
 
 <div class="w-full mb-5 flex flex-col justify-start">
@@ -11,8 +35,13 @@
 	</p>
 	<div class="w-full pt-4 md:px-10 flex justify-start">
 		<ul>
-			{#each socialMedia as { name, url }}
-				<li class="my-2 text-xl"><a href={url} target="_blank" rel="noreferrer">{name}</a></li>
+			{#each socialMedia as { name, url, logo }}
+				<li class="my-4 text-xl">
+					<a class="flex flex-row " href={url} target="_blank" rel="noreferrer"
+						><Icon data={logoLoop(logo)} scale={2} />
+						<p class="ml-4">{name}</p></a
+					>
+				</li>
 			{/each}
 		</ul>
 	</div>
