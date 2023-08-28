a6bfe9ffbb88835e3f1535d6b2ec4893986ff34e
Dan Lubbers <lubbers.dan@gmail.com>
2023-08-28 14:51:47 -0400

    chore: updated styling configs

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,10 @@
-import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
-
+import { vitePreprocess } from '@sveltejs/kit/vite';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [
-		preprocess({
-			postcss: true
-		})
-	],
 	kit: {
 		adapter: adapter()
-	}
+	},
+	preprocess: vitePreprocess()
 };
-
 export default config;
